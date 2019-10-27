6dfda36738829ec7c43458d6a80217635d2f70fa
Yordan Georgiev <yordan.georgiev@gmail.com>
2019-10-27 19:17:01 +0200

    add the tmux provisioning to the setup-vim-n-bash.sh

--- src/bash/deployer/setup-bash-n-vim.sh
+++ src/bash/deployer/setup-bash-n-vim.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# usage: 
-# wget https://raw.githubusercontent.com/YordanGeorgiev/ysg-confs/master/src/bash/deployer/setup-bash-n-vim.sh | bash
+# usage:
+# curl https://raw.githubusercontent.com/YordanGeorgiev/ysg-confs/master/src/bash/deployer/setup-bash-n-vim.sh | bash
 main(){
    do_enable_locate
+   do_provision_tmux
    do_provision_vim
    do_provision_bash
 }
@@ -11,8 +12,28 @@ do_enable_locate(){
    sudo updatedb & # because of the locate elflord.vim bellow and just to speed up..
 }
 
+do_provision_tmux(){
+
+   echo 'start ::: provisioning tmux'
+   which tmux 2>/dev/null || {
+      sudo apt-get update
+      sudo apt-get install -y tmux
+   }
+   mkdir -p ~/.tmux/plugins
+   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tmux-copycat
+   echo verify the tmux plugins
+   find ~/.tmux/plugins -type d -maxdepth 2
+
+   test -f ~/.tmux.conf && cp -v ~/.tmux.conf ~/.tmux.conf.$(date "+%Y%m%d_%H%M%S")
+   wget -O ~/.tmux.conf \
+      https://raw.githubusercontent.com/YordanGeorgiev/ysg-confs/master/.tmux.conf.host-name
+   echo 'stop  ::: provisioning tmux'
+
+}
+
 do_provision_vim(){
-   echo 'start provisioning vim'
+   echo 'start ::: provisioning vim'
    which vim 2>/dev/null || {
       sudo apt-get update
       sudo apt-get install -y vim
@@ -21,20 +42,21 @@ do_provision_vim(){
       sudo apt-get update
       sudo apt-get install -y wget
    }
+   test -f ~/.vimrc && cp -v ~/.vimrc ~/.vimrc.$(date "+%Y%m%d_%H%M%S")
    wget -O ~/.vimrc 'https://raw.githubusercontent.com/YordanGeorgiev/ysg-confs/master/.vimrc.host-name'
    sudo perl -pi -e \
       '$_="$_\nhi LineNr term=bold cterm=NONE ctermfg=DarkGrey ctermbg=NONE gui=NONE guifg=DarkGrey guibg=NONE"
-      if $. == 27' $(locate elflord.vim)
-   echo 'stop provisioning vim'
+      if $. == 27' $(locate elflord.vim) &
+   echo 'stop ::: provisioning vim'
 }
 
 do_provision_bash(){
-   echo 'start provisioning bash'
+   echo 'start ::: fetching bash_opts'
    wget -O ~/.bash_opts.`hostname -s` \
       'https://raw.githubusercontent.com/YordanGeorgiev/ysg-confs/master/.bash_opts.host-name'
 
    echo "source ~/.bash_opts.`hostname -s`"
-   echo 'stop provisioning bash'
+   echo 'stop  ::: fetching bash_opts'
 }
 
 # Action !!!
